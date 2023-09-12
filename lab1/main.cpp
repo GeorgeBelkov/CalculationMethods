cba4ac8a64f70a9f0e8dccd934f0d2c3e39f9402
Ysan Asanov <saasaaan480@gmail.com>
2023-09-12 19:51:11 +0200

    QR разложение (не полностью)

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,23 +4,22 @@
 int main()
 {
     Matrix<double> A(3, 3, "/home/ysan/numer/CalculationMethods/lab1/matrixA.txt");
-    Matrix<double> C(3, 3, "/home/ysan/numer/CalculationMethods/lab1/matrixC.txt");
-    A.output();
-    Matrix<double> B = A;
-    B.output();
-    B = C;
-    B.output();
-    B = B;
-    B[0] = A[1];
-    B.output();
-    Matrix<double> D = multiply(A, B);
-    D.output();
-    D.transpoce();
-    D.output();
-    Matrix<double> E = makeE<double>(4);
+    //Matrix<double> C(3, 3, "/home/ysan/numer/CalculationMethods/lab1/matrixC.txt");
+    
+    Matrix<double> Q(3,3),R(3,3);
+    QR(A,Q,R);
+    Q.output();
+    R.output();
+
+    Matrix<double> Q_ = Q;
+    Q_.transpoce();
+    Matrix<double> E = multiply(Q_,Q);
     E.output();
-    C.mulRowAndScalar(1, 3);
-    C.output();
-    C.addRow(1, 2);
-    C.output();
+    //Matrix<double> Temp = makeRot(0,1,A);
+    //Matrix<double> M1 = multiply(Temp,A);
+    //Matrix<double> Temp1 = makeRot(0,2,M1);
+    //Matrix<double> M2 = multiply(Temp1,M1);
+    //Temp.output();
+    //M2.output();
 }
+    
