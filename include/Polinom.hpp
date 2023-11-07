97be6c489f5e31ea07f35c490b914efea13c012d
George <TaTuVampire@yandex.ru>
2023-11-08 00:36:06 +0300

    Сырой вариант интерполяции Лагранжем

--- include/Polinom.hpp
+++ include/Polinom.hpp
@@ -2,13 +2,43 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <numeric>
 #include <iostream>
 #include <algorithm>
+
+
+constexpr static size_t NODES_COUNT = 5;
+
+
+template<typename T>
+class Grid;
 
 
 enum class GridType {
     UNIFORM = 0,
     CHEBYSHEVSKAYA
+};
+
+
+template<typename T>
+struct InterpolationTable
+{
+    std::vector<std::pair<T, T>> table;
+
+    InterpolationTable(Grid<T> const& grid, std::function<T(T)> const& function)
+    {
+        table.resize(grid.getGrid().size());
+        for (size_t i = 0; i < table.size(); i++)
+            table[i] = std::make_pair(grid.getGrid()[i], function(grid.getGrid()[i]));
+    }
+
+    void printTable() const
+    {
+        static std::ofstream table_fout("tableInfo.txt");
+        for (auto& pair : table)
+            table_fout << "x: " << pair.first << "; y: " << pair.second << "\n";
+        table_fout << "\n\n";
+    }
 };
 
 
@@ -34,6 +64,9 @@ public:
 
     // Методы класса
     void print() const;
+
+    // Get, Set методы
+    std::vector<T> getGrid() const { return grid; }
 };
 
 
@@ -125,6 +158,7 @@ private:
 
 public:
     // Конструкторы
+    Polinom() = default;
     explicit Polinom(size_t deg) { polinom_coeffs.reserve(deg + 1); }
     Polinom(Polinom<T> const& other): polinom_coeffs(other.polinom_coeffs) {}
     Polinom(Polinom<T>&& other) noexcept: polinom_coeffs(std::move(other.polinom_coeffs)) {}
@@ -138,10 +172,18 @@ public:
     Polinom<T>& operator=(Polinom<T>&& other) noexcept;
 
 
+    // Методы класса
+    void print() const;
+
+
     // Дружественные функции класса
 
+    // Комбинирует индексы от 0 до polinom_deg с помощью ф-лы: C из polinom_deg по number
     template<typename V>
-    friend Polinom<V> LarangeInterpolation();
+    friend V getCoeff(Grid<V> const& grid, uint16_t polinom_deg, uint16_t number);
+
+    template<typename V>
+    friend void LagrangeInterpolation(Polinom<V>& Lagrange_polinom, Grid<V> const& grid, InterpolationTable<V> const& table);
 
     template<typename V>
     friend std::vector<Polinom<V>> SplineInterpolation();
@@ -164,4 +206,72 @@ Polinom<T>& Polinom<T>::operator=(Polinom<T>&& other) noexcept
     polinom_coeffs.clear();
     polinom_coeffs = std::move(other.polinom_coeffs);
     return *this;
+}
+
+
+template<typename T>
+void Polinom<T>::print() const
+{
+    static std::ofstream fout("InterpolationInfo.txt");
+
+    for (auto& coeff : polinom_coeffs)
+        fout << coeff << " ";
+
+    fout << "\n\n";
+}
+
+
+template<typename V>
+V getCoeff(Grid<V> const& grid, uint16_t polinom_deg, uint16_t number)
+{
+    // Хранит всевозможные комбинации произведений длины number
+    std::vector<V> combinations;
+
+    // Битмаска
+    std::string bitmask(number, 1);
+    bitmask.resize(polinom_deg + 1, 0);
+    
+    do {
+        V combination = 1;
+        for (size_t i = 0; i <= polinom_deg; ++i)
+            if (bitmask[i]) combination *= grid.getGrid()[i];
+
+        combinations.push_back(combination);
+    } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+
+    // Возвращаем сумму (то есть коэффицент) при (polinom_deg - number) степени.
+    return std::accumulate(combinations.begin(), combinations.end(), 1);
+}
+
+
+template<typename V>
+void LagrangeInterpolation(Polinom<V>& Lagrange_polinom, Grid<V> const& grid, InterpolationTable<V> const& table)
+{
+    size_t polinom_deg = table.table.size();
+    Lagrange_polinom.polinom_coeffs.resize(polinom_deg);
+    auto grid_copy = grid.getGrid();
+    // Хранит базис, по которому строится полином Лагранжа
+    std::vector<Polinom<V>> basic_functions(polinom_deg);
+
+    for (size_t k = 0; k < polinom_deg; k++)
+    {
+        // берем k-ю базисную функцию
+        auto& base_function = basic_functions[k];
+        base_function.polinom_coeffs.resize(polinom_deg, 1);
+        
+        V multiplier = 1;
+        for (size_t i = 0; ((i != k) && (i < polinom_deg)); i++)
+            multiplier *= (grid_copy[k] - grid_copy[i]);
+        
+
+        // заполняем ее коэффиценты
+        base_function.polinom_coeffs[0] *= multiplier * table.table[polinom_deg - 1].second;
+        base_function.polinom_coeffs[1] = ((-multiplier) * std::accumulate(grid_copy.begin(), grid_copy.end(), 1) * table.table[polinom_deg - 2].second);
+        for (size_t i = 2; i < polinom_deg; i++)
+            // i-й коэффицент = (-1)^i * множитель * сумму комбинаций.
+            base_function.polinom_coeffs[i] = std::pow(-1, i) * multiplier * getCoeff<V>(grid, polinom_deg, i) * table.table[polinom_deg - (i + 1)].second;
+        
+        for (size_t i = 0; i < Lagrange_polinom.polinom_coeffs.size(); i++)
+            Lagrange_polinom.polinom_coeffs[i] += base_function.polinom_coeffs[i];
+    }
 }
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -2,17 +2,41 @@
 
 
 using type_t = double;
-constexpr static size_t NODES_COUNT = 5;
+
+
+template<typename V>
+V testFunction(V x)
+{
+    // return 1.0 / std::atan(1 + 10 * std::pow(x, 2));
+    // return 1.0 / (1 + std::pow(x, 2));
+    return std::pow(x, 2);
+}
 
 
 int main()
 {
-    std::pair<type_t, type_t> uniform_segment(1, 3);
-    std::pair<type_t, type_t> chebyshev_segment(1, 3);
+    std::pair<type_t, type_t> uniform_segment(-1, 1);
+    std::pair<type_t, type_t> chebyshev_segment(-1, 1);
     Grid<type_t> uniform_grid(NODES_COUNT, uniform_segment, GridType::UNIFORM);
     Grid<type_t> chebyshev_grid(NODES_COUNT, chebyshev_segment, GridType::CHEBYSHEVSKAYA);
 
-
+    // Вывод получившихся сеток в файл.
     uniform_grid.print();
     chebyshev_grid.print();
+
+    InterpolationTable<type_t> uniform_grid_table(uniform_grid, testFunction<type_t>);
+    InterpolationTable<type_t> chebyshev_grid_table(chebyshev_grid, testFunction<type_t>);
+
+    // Вывод получившихся таблиц в файл.
+    uniform_grid_table.printTable();
+    chebyshev_grid_table.printTable();
+
+    Polinom<type_t> Lagrange_with_uniform_grid(NODES_COUNT - 1);
+    Polinom<type_t> Lagrange_with_chebyshev_grid(NODES_COUNT - 1);
+
+    LagrangeInterpolation<type_t>(Lagrange_with_uniform_grid, uniform_grid, uniform_grid_table);
+    LagrangeInterpolation<type_t>(Lagrange_with_chebyshev_grid, chebyshev_grid, chebyshev_grid_table);
+
+    Lagrange_with_uniform_grid.print();
+    Lagrange_with_chebyshev_grid.print();
 }
