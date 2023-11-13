e64ff1c490cd6ec48bbaf83c2b240c8ee089510f
2fab4u <139163888+GeorgeBelkov@users.noreply.github.com>
2023-11-13 23:21:34 +0300

    Сделана и протестирована на основных и дополнительных заданиях лабораторная 3. Интерполяция.

--- include/Polinom.hpp
+++ include/Polinom.hpp
@@ -1,141 +1,443 @@
+#pragma once
+
 #include <cmath>
-#include <string>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <numeric>
 #include <iostream>
 #include <algorithm>
-#include <filesystem>
+
+
+constexpr static size_t NODES_COUNT = 10;
+constexpr static double epsilon = 1e-8;
 
 
 template<typename T>
-class InterpolationPolinom
-{
-private:
-    std::vector<std::pair<T, T>> table;
-    std::vector<T> polinom_coeffs; 
+class Grid;
 
-public:
+template<typename T>
+struct InterpolationTable;
 
-    // Конструктор
-    InterpolationPolinom(std::vector<T> const& nodes, std::function<T(T)> function)
-    {
-        fillTable<T>(this->table, nodes, function);
-        auto supportive_polinoms = makeSupportivePolinoms();
-        polinom_coeffs.resize(nodes.size());
 
-        size_t iter = 0;
-        for (auto& polinom : supportive_polinoms)
-        {
-            for (auto coeff : polinom)
-            {
-                polinom_coeffs[iter] += (coeff * table[iter].second);
-                iter++;
-            }
-            iter = 0;
-        }
-    }
-
-    std::vector<T> getPolinomCoeffs() { return polinom_coeffs; }
-
-    // для многочленов Лагранжа - создает вспомогательные полиномы C_{k}(x) и возвращает их вектором.
-    std::vector<std::vector<T>> makeSupportivePolinoms();
-
-    // Заполнение сетки pair.first - узлы, pair.second - значение сеточной функции в узле.
-    template<typename V>
-    friend void fillTable(std::vector<std::pair<V, V>>& table, std::vector<V> const& nodes, std::function<V(V)> function);
-
-    // Вывод таблицы значений (x_i, y_i), по которым производится интерполирование.
-    template<typename V>
-    friend void outputTable(std::vector<std::pair<V, V>>& table);
-
-    // Комбинирует индексы от 0 до polinom_deg с помощью ф-лы: C из polinom_deg по number
-    template<typename V>
-    friend std::vector<std::vector<size_t>> comb(short polinom_deg, short number);
+enum class GridType {
+    UNIFORM = 0,
+    CHEBYSHEVSKAYA
 };
 
 
+// Функция возвращает прогоночные коэффиценты alpha[i], betta[i] для нахождения c_i в кубическом сплайне.
 template<typename V>
-void fillTable(std::vector<std::pair<V, V>>& table, std::vector<V> const& nodes, std::function<V(V)> function)
+std::pair<V, V> getRunThroughCoeffs(InterpolationTable<V> const& table, std::vector<std::pair<V, V>> const& coeffs, size_t index)
 {
-    for (size_t i = 0; i < nodes.size(); i++)
+    // можно оптимальнее (избавиться от двух аддитивных и двух мультипликативных)
+    // g_i and g_(i-1)
+    auto g_i = (table.table[index].second - table.table[index - 1].second) / (table.table[index].first - table.table[index - 1].first);
+    auto g_i_prev = (table.table[index - 1].second - table.table[index - 2].second) / (table.table[index - 1].first - table.table[index - 2].first);
+    if (index == 2)
     {
-        std::pair<V, V> pair;
-        pair.first = nodes[i];
-        pair.second = function(nodes[i]);
-        table.push_back(pair);
+        auto alpha = (table.table[index].first - table.table[index - 1].first) / (-2 * (table.table[index].first - table.table[index - 2].first));
+        auto betta = (3 * (g_i - g_i_prev)) / (2 * (table.table[index].first - table.table[index - 2].first));
+
+        return std::make_pair(alpha, betta);
     }
-}
+    else if (index == table.table.size() - 1)
+    {
+        auto betta = (-3 * (g_i - g_i_prev) + (table.table[index - 1].first - table.table[index - 2].first) * coeffs[index - 3].second) /
+                     (-2 * (table.table[index].first - table.table[index - 2].first) -
+                      (table.table[index - 1].first - table.table[index - 2].first) * coeffs[index - 3].first);
+        return std::make_pair(0, betta);
+    }
+    else
+    {
+        auto alpha = (table.table[index].first - table.table[index - 1].first) /
+                     (-2 * (table.table[index].first - table.table[index - 2].first) -
+                      (table.table[index - 1].first - table.table[index - 2].first) * coeffs[index - 3].first);
 
-
-template<typename V>
-std::vector<std::vector<size_t>> comb(short polinom_deg, short number)
-{
-    std::vector<std::vector<size_t>> combinations;
-    std::string bitmask(number, 1);
-    bitmask.resize(polinom_deg + 1, 0);
-    
-    do {
-        std::vector<size_t> temp;
-        for (size_t i = 0; i <= polinom_deg; ++i)
-        {
-            if (bitmask[i]) temp.push_back(i);
-        }
-        combinations.push_back(temp);
-    } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
-
-    return combinations;
+        auto betta = (-3 * (g_i - g_i_prev) + (table.table[index - 1].first - table.table[index - 2].first) * coeffs[index - 3].second) /
+                     (-2 * (table.table[index].first - table.table[index - 2].first) +
+                      -1 * (table.table[index - 1].first - table.table[index - 2].first) * coeffs[index - 3].first);
+        return std::make_pair(alpha, betta);
+    }
 }
 
 
 template<typename T>
-std::vector<std::vector<T>> InterpolationPolinom<T>::makeSupportivePolinoms()
+struct InterpolationTable
 {
-    std::vector<std::vector<T>> polinoms;
+    std::vector<std::pair<T, T>> table;
 
-    for (size_t i = 0; i < this->table.size(); i++)
+    InterpolationTable(Grid<T> const& grid, std::function<T(T)> const& function)
     {
-        std::vector<T> polinom(this->table.size(), 1);
-        T multiplier = 1;
-        
-        for (size_t j = 0; j < this->table.size(); j++)
-            if (j != i)
-                multiplier *= (this->table[i].first - this->table[j].first);
-
-        for (size_t j = 0; j < this->table.size(); j++)
-        {
-            if (j == i)
-                continue;
-            else
-            {
-                auto temp = comb<T>(this->table.size(), j);
-                T sum = 0;
-                for (auto& indexes : temp)
-                {
-                    T mul = 1;
-                    for (auto index : indexes)
-                    {
-                        mul *= this->table[index].first; 
-                    }
-                    sum += mul;
-                    mul = 1;
-                }
-                polinom[j] = sum;
-            }
-        }
-
-        for (auto& coeff : polinom)
-            coeff *= multiplier;
-
-        polinoms.push_back(polinom);
+        table.resize(grid.getGrid().size());
+        for (size_t i = 0; i < table.size(); i++)
+            table[i] = std::make_pair(grid.getGrid()[i], function(grid.getGrid()[i]));
     }
+
+    void printTable() const
+    {
+        static std::ofstream table_fout("tableInfo.txt");
+        static bool is_closed = false;
+
+        if (is_closed)
+            table_fout.open("tableInfo.txt", std::ios::out);
+
+        for (auto& pair : table)
+            table_fout << "x: " << pair.first << " y: " << pair.second << "\n";
+        table_fout << "\n\n";
+
+        table_fout.close();
+        is_closed = true;
+    }
+};
+
+
+template<typename T>
+class Grid
+{
+private:
+    std::vector<T> grid;
+    GridType grid_type;
+public:
+    // Конструкторы
+    Grid(size_t nodes_count, std::pair<T, T> segment, GridType grid_tp);
+    Grid(Grid<T> const& other): grid(other.grid), grid_type(other.grid_type) {}
+    Grid(Grid<T>&& other) noexcept: grid(std::move(other.grid)), grid_type(other.grid_type) {}
+
+    // Деструкторы
+    ~Grid() = default;
     
-    return polinoms;
+    // Оператор присваивания
+    Grid<T>& operator=(Grid<T> const& other);
+    // Оператор перемещения
+    Grid<T>& operator=(Grid<T>&& other) noexcept;
+
+    // Методы класса
+    void print() const;
+
+    // Get, Set методы
+    std::vector<T> getGrid() const { return grid; }
+};
+
+
+template<typename T>
+Grid<T>::Grid(size_t nodes_count, std::pair<T, T> segment, GridType grid_tp)
+{
+    grid.resize(nodes_count);
+    grid_type = grid_tp;
+    switch (grid_tp)
+    {
+    // Если нужна равномерная сетка
+    case GridType::UNIFORM:
+    {
+        auto dist = std::abs(segment.first - segment.second) / (nodes_count - 1);
+        uint16_t iter = 0;
+        for (auto& node : grid)
+        {
+            node = segment.first + iter * dist;
+            iter++;
+        }
+        break;
+    }
+    // Если нужна Чебышевская сетка
+    case GridType::CHEBYSHEVSKAYA:
+    {
+        std::vector<T> chebyshev_nodes;
+        for (size_t k = 0; k < nodes_count; k++)
+        {
+            auto chebyshev_node = std::cos(((2 * (k + 1) - 1) * M_PI) / (2 * nodes_count));
+            grid[k] = (0.5 * (segment.first + segment.second) + 0.5 * (segment.second - segment.first) * chebyshev_node);
+        }
+        std::sort(grid.begin(), grid.end());
+        break;
+    }
+    default:
+    {
+        std::cout << "\nChoose correct grid type!\n";
+        exit(1);
+    }
+    }
 }
 
 
+template<typename T>
+Grid<T>& Grid<T>::operator=(Grid<T> const& other)
+{
+    grid.clear();
+    grid = other.grid;
+    grid_type = other.grid_type;
+    return *this;
+}
 
 
+template<typename T>
+Grid<T>& Grid<T>::operator=(Grid<T>&& other) noexcept
+{
+    grid.clear();
+    grid = std::move(other.grid);
+    grid_type = other.grid_type;
+    return *this;
+}
 
 
+// Методы класса
 
 
+template<typename T>
+void Grid<T>::print() const
+{
+    static std::ofstream fout("gridInfo.txt");
 
+    if (grid_type == GridType::CHEBYSHEVSKAYA)
+        fout << "Chebyshevskaya grid:\n";
+    else
+        fout << "Uniform grid:\n";
+    
+    for (auto& node : grid)
+        fout << node << " ";
+
+    fout << "\n\n";
+}
+
+
+template<typename T>
+class Polinom
+{
+private:
+    std::vector<T> polinom_coeffs;
+
+public:
+    // Конструкторы
+    Polinom() = default;
+    explicit Polinom(size_t deg) { polinom_coeffs.reserve(deg + 1); }
+    Polinom(Polinom<T> const& other): polinom_coeffs(other.polinom_coeffs) {}
+    Polinom(Polinom<T>&& other) noexcept: polinom_coeffs(std::move(other.polinom_coeffs)) {}
+
+    // Деструкторы
+    ~Polinom() = default;
+    
+    // Оператор присваивания
+    Polinom<T>& operator=(Polinom<T> const& other);
+    // Оператор перемещения
+    Polinom<T>& operator=(Polinom<T>&& other) noexcept;
+
+
+    // Методы класса
+    void print() const;
+    void printSpline() const;
+
+
+    // Дружественные функции класса
+
+    // Комбинирует индексы от 0 до polinom_deg с помощью ф-лы: C из polinom_deg по number
+    template<typename V>
+    friend V getCoeff(Grid<V> const& grid, uint16_t polinom_deg, uint16_t number, size_t excluded);
+
+    template<typename V>
+    friend void LagrangeInterpolation(Polinom<V>& Lagrange_polinom, Grid<V> const& grid, InterpolationTable<V> const& table);
+
+    template<typename V>
+    friend void CubicSplineInterpolation(std::vector<Polinom<V>>& spline, InterpolationTable<V> const& table);
+
+};
+
+
+template<typename T>
+Polinom<T>& Polinom<T>::operator=(Polinom<T> const& other)
+{
+    polinom_coeffs.clear();
+    polinom_coeffs(other.polinom_coeffs);
+    return *this;
+}
+
+
+template<typename T>
+Polinom<T>& Polinom<T>::operator=(Polinom<T>&& other) noexcept
+{
+    polinom_coeffs.clear();
+    polinom_coeffs = std::move(other.polinom_coeffs);
+    return *this;
+}
+
+
+template<typename T>
+void Polinom<T>::print() const
+{
+    static std::ofstream fout("LagrangeInterpolationInfo.txt");
+    static bool is_closed = false;
+
+    if (is_closed)
+        fout.open("LagrangeInterpolationInfo.txt", std::ios::out);
+    
+
+    for (size_t i = 0; i < polinom_coeffs.size(); i++)
+    {
+        if (i == polinom_coeffs.size() - 1)
+            fout << polinom_coeffs[i];
+        else
+            fout << polinom_coeffs[i] << " ";
+    }
+
+    fout << "\n\n";
+
+    fout.close();
+    is_closed = true;
+}
+
+
+template<typename T>
+void Polinom<T>::printSpline() const
+{
+    static std::ofstream fout("SplineInterpolationInfo.txt");
+    static size_t id = 1;
+    static bool is_closed = false;
+
+    if (is_closed)
+        fout.open("SplineInterpolationInfo.txt", std::ios::out);
+
+    fout << id << ": ";
+    for (size_t i = 0; i < polinom_coeffs.size(); i++)
+    {
+        if (i == polinom_coeffs.size() - 1)
+            fout << polinom_coeffs[i];
+        else
+            fout << polinom_coeffs[i] << " ";
+    }
+    
+    fout << "\n";
+
+    id++;
+    if (id == NODES_COUNT)
+    {
+        id = 1;
+        fout << "\n\n";
+        fout.close();
+        is_closed = true;
+    }
+}
+
+
+template<typename V>
+V getCoeff(Grid<V> const& grid, uint16_t polinom_deg, uint16_t number, size_t excluded)
+{
+    // Хранит всевозможные комбинации произведений длины number
+    std::vector<V> combinations;
+
+    // Битмаска
+    std::string bitmask(number, 1);
+    bitmask.resize(polinom_deg + 1, 0);
+    auto grid_copy = grid.getGrid();
+    
+    do {
+        V combination = 1;
+        bool push_flag = true;
+        for (size_t i = 0; i <= polinom_deg; ++i)
+            if (bitmask[i])
+            {
+                if (i == excluded)
+                {
+                    push_flag = false;
+                    break;
+                }
+                combination *= grid_copy[i];
+            }
+
+        if (push_flag)
+            combinations.push_back(combination);
+        
+
+    } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+
+    // Возвращаем сумму (то есть коэффицент) при (polinom_deg - number) степени.
+    V sum = 0;
+    for (auto& elem : combinations)
+        if (std::abs(elem) > epsilon)
+            sum += elem;
+    return sum;
+}
+
+
+template<typename V>
+void LagrangeInterpolation(Polinom<V>& Lagrange_polinom, Grid<V> const& grid, InterpolationTable<V> const& table)
+{
+    size_t polinom_deg = table.table.size();
+    Lagrange_polinom.polinom_coeffs.resize(polinom_deg);
+    auto grid_copy = grid.getGrid();
+    // Хранит базис, по которому строится полином Лагранжа
+    std::vector<Polinom<V>> basic_functions(polinom_deg);
+
+    for (size_t k = 0; k < polinom_deg; k++)
+    {
+        // берем k-ю базисную функцию
+        auto& base_function = basic_functions[k];
+        base_function.polinom_coeffs.resize(polinom_deg, 1);
+        
+        V multiplier = 1, sum = 0;
+        for (size_t i = 0; i < polinom_deg; i++)
+            if (i != k)
+            {
+                multiplier *= (grid_copy[k] - grid_copy[i]);
+                sum += grid_copy[i];
+            }
+        
+
+        // заполняем ее коэффиценты
+        base_function.polinom_coeffs[0] *= (1 / multiplier) * table.table[k].second;
+        base_function.polinom_coeffs[1] *= (-(1 / multiplier) * sum * table.table[k].second);
+        for (size_t i = 2; i < polinom_deg; i++)
+            // i-й коэффицент = (-1)^i * множитель * сумму комбинаций.
+            base_function.polinom_coeffs[i] *= std::pow(-1, i) * (1 / multiplier) * getCoeff<V>(grid, polinom_deg, i, k) * table.table[k].second;
+        
+        for (size_t i = 0; i < polinom_deg; i++)
+            Lagrange_polinom.polinom_coeffs[i] += base_function.polinom_coeffs[i];
+    }
+    for (auto& elem : Lagrange_polinom.polinom_coeffs)
+        if (std::abs(elem) < epsilon)
+            elem = 0;
+}
+
+
+template<typename V>
+void CubicSplineInterpolation(std::vector<Polinom<V>>& spline, InterpolationTable<V> const& table)
+{
+    std::vector<std::pair<V, V>> coeffs;
+    for (size_t i = 2; i < table.table.size(); i++)
+        coeffs.push_back(getRunThroughCoeffs(table, coeffs, i));
+
+    short j = -1;
+    for (size_t i = table.table.size() - 1; i > 0; i--, j++)
+    {
+        Polinom<V> cubic_polinom;
+        cubic_polinom.polinom_coeffs.resize(4);
+
+        auto g_i = (table.table[i].second - table.table[i - 1].second) / (table.table[i].first - table.table[i - 1].first);
+        auto h_i = table.table[i].first - table.table[i - 1].first;
+
+        V a_i = table.table[i - 1].second, c_i, b_i, d_i;
+        if (i == table.table.size() - 1)
+        {
+            c_i = coeffs[i - 2].second;
+            b_i = g_i - (2 * c_i * h_i) / 3;
+            d_i = (-c_i) / (3 * h_i);
+        }
+        else
+        {
+            c_i = coeffs[i - 2].first * spline[j].polinom_coeffs[1] + coeffs[i - 2].second;
+            b_i = g_i - ((spline[j].polinom_coeffs[1] + 2 * c_i) * h_i) / 3;
+            d_i = (spline[j].polinom_coeffs[1] - c_i) / (3 * h_i);
+        }
+
+        // устанавливаем коэффиценты полинома
+        cubic_polinom.polinom_coeffs[0] = d_i;
+        cubic_polinom.polinom_coeffs[1] = c_i;
+        cubic_polinom.polinom_coeffs[2] = b_i;
+        cubic_polinom.polinom_coeffs[3] = a_i;
+
+        for (auto& elem : cubic_polinom.polinom_coeffs)
+            if (std::abs(elem) < epsilon)
+                elem = 0;
+
+        spline.push_back(cubic_polinom);
+    }
+    std::reverse(spline.begin(), spline.end());
+}
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -4,59 +4,258 @@
 using type_t = double;
 
 
-static constexpr size_t NUMBER_OF_NODES = 5;
-
-
-enum class Grid {
-    UNIFORM = 0,
-    CHEBYSHEVSKAYA
+enum class Task {
+    MAIN_LAGRANGE_UNIFORM = 0,
+    MAIN_LAGRANGE_CHEBYSHEV,
+    MAIN_SPLINE_UNIFORM,
+    MAIN_SPLINE_CHEBYSHEV,
+    ADDITIONAL1,
+    ADDITIONAL2,
 };
+
+
+template<typename V>
+V complexFunction(V x)
+{
+    if (x > (-M_PI - 0.1) && x < 0)
+    {
+        return std::sin(x);
+    }
+    else if (x >= 0 && x <= (1 + 0.1))
+    {
+        return x - std::pow(x, 3) / 3;
+    }
+    
+    
+    return 5;
+}
+
+
+template<typename V>
+V testFunction(V x)
+{
+    // return std::pow(x, 2);                                                                                                               //  +
+    // return 1.0 / (1 + std::pow(x, 2));                                                                                                   //  +
+    // return 1.0 / std::atan(1 + 10 * std::pow(x, 2));                                                                                     //  +
+    // return std::pow(4 * std::pow(x, 3) + 2 * std::pow(x, 2) - 4 * x + 2, std::sqrt(2)) + std::asin(1 / (5 + x - std::pow(x, 2))) - 5;    //  +
+    // return 1.0 / (1 + 25 * std::pow(x, 2));                                                                                                 +
+    return std::sin(x);
+    // return complexFunction(x);
+}
 
 
 template<typename V>
 V square(V x)
 {
-    // return x * x;
-    // return 1 / (1 + x * x);
-    return 1 / std::atan(1 + 10 * x * x);
+    return std::pow(x, 2);
 }
 
 
 template<typename V>
-std::vector<V> createGrid(size_t number_of_nodes, std::pair<V, V> segment, Grid grid)
+V constant(V x)
 {
-    switch (grid)
-    {
-    case Grid::UNIFORM:
-    {
-        std::vector<V> nodes(number_of_nodes);
-        auto step = (std::abs(segment.first) + std::abs(segment.second)) / number_of_nodes;
-
-        for (size_t i = 0; i < number_of_nodes; i++)
-            nodes[i] = (segment.first + step * i);
-
-        return nodes;
-    }
-    case Grid::CHEBYSHEVSKAYA:
-    {
-        std::vector<V> nodes(number_of_nodes);
-        /*TODO*/
-        return nodes;
-    }
-    default:
-        break;
-    }
-    return std::vector<V>(number_of_nodes, 0);
+    return 1;
 }
+
+
+template<typename V>
+void interpolant(std::function<V(V)>const& func, std::pair<V, V> segment)
+{
+    constexpr static size_t ITERS = 100000;
+    static std::ofstream fout("functionInfo.txt");
+    auto dist = std::abs(segment.first - segment.second) / (ITERS - 1);
+    for (size_t i = 0; i < ITERS; i++)
+        fout << (segment.first + i * dist) << " " << func(segment.first + i * dist) << "\n";
+
+    fout.close();
+}
+
 
 
 int main()
 {
-    std::pair<type_t, type_t> segment(-3, 3);
-    auto nodes = createGrid(NUMBER_OF_NODES, segment, Grid::UNIFORM);
-    InterpolationPolinom<type_t> Lagrange(nodes, square<type_t>);
-    auto polinom = Lagrange.getPolinomCoeffs();
-    for (auto coeff : polinom)
-        std::cout << coeff << " ";
-    std::cout << std::endl;
+    Task task = Task::MAIN_LAGRANGE_UNIFORM;
+    std::string bash_command;
+    switch (task)
+    {
+    case Task::MAIN_LAGRANGE_UNIFORM:
+    {
+        std::pair<type_t, type_t> uniform_segment(0, 10);
+        Grid<type_t> uniform_grid(NODES_COUNT, uniform_segment, GridType::UNIFORM);
+
+        interpolant<type_t>(testFunction<type_t>, uniform_segment);
+
+        // Вывод получившейся сетки в файл.
+        uniform_grid.print();
+
+        InterpolationTable<type_t> uniform_grid_table(uniform_grid, testFunction<type_t>);
+
+        // Вывод получившейся таблицы в файл.
+        uniform_grid_table.printTable();
+
+        Polinom<type_t> Lagrange_with_uniform_grid(NODES_COUNT - 1);
+        LagrangeInterpolation<type_t>(Lagrange_with_uniform_grid, uniform_grid, uniform_grid_table);
+        Lagrange_with_uniform_grid.print();
+        
+        bash_command = "python3 plotter.py lagrange";
+
+        break;
+    }
+    case Task::MAIN_LAGRANGE_CHEBYSHEV:
+    {
+        std::pair<type_t, type_t> chebyshev_segment(-M_PI, 1);
+        Grid<type_t> chebyshev_grid(NODES_COUNT, chebyshev_segment, GridType::CHEBYSHEVSKAYA);
+
+        interpolant<type_t>(testFunction<type_t>, chebyshev_segment);
+
+        // Вывод получившейся сетки в файл.
+        chebyshev_grid.print();
+
+        InterpolationTable<type_t> chebyshev_grid_table(chebyshev_grid, testFunction<type_t>);
+
+        // Вывод получившейся таблицы в файл.
+        chebyshev_grid_table.printTable();
+
+        Polinom<type_t> Lagrange_with_chebyshev_grid(NODES_COUNT - 1);
+        LagrangeInterpolation<type_t>(Lagrange_with_chebyshev_grid, chebyshev_grid, chebyshev_grid_table);
+        Lagrange_with_chebyshev_grid.print();
+        
+        bash_command = "python3 plotter.py lagrange";
+
+        break;
+    }
+    case Task::MAIN_SPLINE_UNIFORM:
+    {
+        std::pair<type_t, type_t> uniform_segment(-M_PI, 1);
+        Grid<type_t> uniform_grid(NODES_COUNT, uniform_segment, GridType::UNIFORM);
+
+        interpolant<type_t>(testFunction<type_t>, uniform_segment);
+
+        // Вывод получившейся сетки в файл.
+        uniform_grid.print();
+
+        InterpolationTable<type_t> uniform_grid_table(uniform_grid, testFunction<type_t>);
+
+        // Вывод получившейся таблицы в файл.
+        uniform_grid_table.printTable();
+
+        std::vector<Polinom<type_t>> Spline_with_uniform_grid;
+
+        CubicSplineInterpolation(Spline_with_uniform_grid, uniform_grid_table);
+
+        for (auto& polinom : Spline_with_uniform_grid)
+           polinom.printSpline();
+
+        bash_command = "python3 plotter.py spline";
+
+        break;
+    }
+    case Task::MAIN_SPLINE_CHEBYSHEV:
+    {
+        std::pair<type_t, type_t> chebyshev_segment(-1, 1);
+        Grid<type_t> chebyshev_grid(NODES_COUNT, chebyshev_segment, GridType::CHEBYSHEVSKAYA);
+
+        interpolant<type_t>(testFunction<type_t>, chebyshev_segment);
+
+        // Вывод получившейся сетки в файл.
+        chebyshev_grid.print();
+
+        InterpolationTable<type_t> chebyshev_grid_table(chebyshev_grid, testFunction<type_t>);
+
+        // Вывод получившейся таблицы в файл.
+        chebyshev_grid_table.printTable();
+
+        std::vector<Polinom<type_t>> Spline_with_chebyshev_grid;
+
+        CubicSplineInterpolation(Spline_with_chebyshev_grid, chebyshev_grid_table);
+
+        for (auto& polinom : Spline_with_chebyshev_grid)
+            polinom.printSpline();
+        
+        bash_command = "python3 plotter.py spline";
+
+        break;
+    }
+    case Task::ADDITIONAL1:
+    {
+        // Флажок, 1 - если интерполяция сплайном проводится на равномерной сетке.
+        bool is_grid_uniform = false;
+
+        if (is_grid_uniform)
+        {
+            std::pair<type_t, type_t> uniform_segment(-100, 100);
+            Grid<type_t> uniform_grid(NODES_COUNT, uniform_segment, GridType::UNIFORM);
+            InterpolationTable<type_t> uniform_grid_table(uniform_grid, square<type_t>);
+
+            interpolant<type_t>(square<type_t>, uniform_segment);
+
+            // Вывод получившейся сетки в файл.
+            uniform_grid.print();
+            
+            // Вывод получившейся таблицы в файл.
+            uniform_grid_table.printTable();
+
+            std::vector<Polinom<type_t>> Spline_with_uniform_grid;
+            CubicSplineInterpolation(Spline_with_uniform_grid, uniform_grid_table);
+
+            for (auto& polinom : Spline_with_uniform_grid)
+                polinom.printSpline();
+
+        }
+        else
+        {
+            std::pair<type_t, type_t> chebyshev_segment(-5, 5);
+            Grid<type_t> chebyshev_grid(NODES_COUNT, chebyshev_segment, GridType::CHEBYSHEVSKAYA);
+            InterpolationTable<type_t> chebyshev_grid_table(chebyshev_grid, square<type_t>);
+
+            interpolant<type_t>(square<type_t>, chebyshev_segment);
+
+            // Вывод получившейся сетки в файл.
+            chebyshev_grid.print();
+
+            // Вывод получившейся таблицы в файл.
+            chebyshev_grid_table.printTable();
+
+            std::vector<Polinom<type_t>> Spline_with_chebyshev_grid;
+            CubicSplineInterpolation(Spline_with_chebyshev_grid, chebyshev_grid_table);
+
+            for (auto& polinom : Spline_with_chebyshev_grid)
+                polinom.printSpline();
+
+        }
+
+        bash_command = "python3 plotter.py spline";
+
+        break;
+    }
+    case Task::ADDITIONAL2:
+    {
+        std::pair<type_t, type_t> uniform_segment(-10, 10);
+        Grid<type_t> uniform_grid(NODES_COUNT, uniform_segment, GridType::UNIFORM);
+        InterpolationTable<type_t> uniform_grid_table(uniform_grid, constant<type_t>);
+
+        interpolant<type_t>(constant<type_t>, uniform_segment);
+
+        // Вывод получившейся сетки в файл.
+        uniform_grid.print();
+
+        // Вывод получившейся таблицы в файл.
+        uniform_grid_table.printTable();
+
+        Polinom<type_t> Lagrange_with_uniform_grid(NODES_COUNT - 1);
+
+        LagrangeInterpolation<type_t>(Lagrange_with_uniform_grid, uniform_grid, uniform_grid_table);
+
+        Lagrange_with_uniform_grid.print();
+
+        bash_command = "python3 plotter.py lagrange";
+
+        break;
+    }
+    default:
+        break;
+    }
+
+    std::cout << "python!\n";
+    std::system(bash_command.c_str());
 }
